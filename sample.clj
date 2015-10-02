13cd1f5ba22020cb2e5e9f922ba7691b2800b917
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-10-02 02:16:35 -0400

    initial commit

---
+++ sample.clj
@@ -0,0 +1,26 @@
+(def metro (metronome 128))
+(metro)
+(metro 100)
+
+(defn player [beat]
+  (at (metro beat) (kick))
+  (at (metro (+ 0.5 beat)) (c-hat))
+  (apply-by (metro (inc beat)) #'player (inc beat) []))
+
+(definst kick [freq 120 dur 0.3 width 0.5]
+  (let [freq-env (* freq (env-gen (perc 0 (* 0.99 dur))))
+        env (env-gen (perc 0.01 dur) 1 1 0 1 FREE)
+        sqr (* (env-gen (perc 0 0.01)) (pulse (* 2 freq) width))
+        src (sin-osc freq-env)
+        drum (+ sqr (* env src))]
+    (compander drum drum 0.2 1 0.1 0.01 0.01)))
+
+(definst c-hat [amp 0.3 t 0.04]
+  (let [env (env-gen (perc 0.001 t) 1 1 0 1 FREE)
+        noise (white-noise)
+        sqr (* (env-gen (perc 0.01 0.04)) (pulse 880 0.2))
+        filt (bpf (+ sqr noise) 9000 0.5)]
+    (* amp env filt)))
+
+(player (metro))
+(stop)
